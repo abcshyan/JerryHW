b9e383c784ef9f039b7f2661cea4f0574ce3dbe4
abcshyan <37055193+abcshyan@users.noreply.github.com>
2018-04-29 16:56:37 +0800

    0416

--- 0416/ntust/Scripts/activate.bat
+++ 0416/ntust/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\shuan\Desktop\0416-2\ntust"
+set "VIRTUAL_ENV=C:\Users\shuan\Desktop\0416\ntust"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
